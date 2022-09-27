f22c0590189c2ab8cdefca32ea29c9c2b6cf2c41
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-28 00:14:23 +0300

    commit tree b460c86f21cd3419291a7c31c64f5c96d8e272bd

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,8 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   # GET /birds
   def index
@@ -10,7 +13,9 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+  render json:bird, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid 
+  render json: {error: :invalid.record.errors}, status: :unprocessable_entity
   end
 
   # GET /birds/:id
@@ -24,6 +29,8 @@ class BirdsController < ApplicationController
     bird = find_bird
     bird.update(bird_params)
     render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json: {error: :invalid.record.errors}, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
